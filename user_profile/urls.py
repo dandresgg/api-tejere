515b43fb7a0ecf9b1bde0ed94b191eab2ae4e0f1
diego <diegogomez268@gmail.com>
2022-06-09 09:52:16 -0500

    create user view and url

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
 
-from user_profile.views import ProfileViewSet
+from user_profile.views import ProfileViewSet, UserViewSet
 
 
 router = routers.DefaultRouter()
 router.register('profile', ProfileViewSet)
+router.register('create', UserViewSet)
 
 
 urlpatterns = [
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,10 +1,21 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from user_profile.models import Profile
-from user_profile.serializers import ProfileSerializer
+from user_profile.serializers import ProfileSerializer, UserSerializer
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny, )
