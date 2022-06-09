e6f20cbc58fed06b1e34a1b12b5bed2223a43ba6
diego <diegogomez268@gmail.com>
2022-06-09 09:01:18 -0500

    iclude user profile url to settings

--- KNITTING/urls.py
+++ KNITTING/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('users/', include('user_profile.urls')),
 ]
