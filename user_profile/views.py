c9f59211250a9a10c3bbc9148928530b573ed32b
diego <diegogomez268@gmail.com>
2022-06-09 12:14:36 -0500

    my auth token with email views

--- user_profile/views.py
+++ user_profile/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.authtoken import views as auth_views
+from rest_framework.compat import coreapi, coreschema
+from rest_framework.schemas import ManualSchema
 
 from user_profile.models import Profile
-from user_profile.serializers import ProfileSerializer, UserSerializer
+from user_profile.serializers import ProfileSerializer, UserSerializer, MyAuthTokenSerializer
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
@@ -19,3 +22,34 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (AllowAny, )
+
+
+class MyAuthToken(auth_views.ObtainAuthToken):
+    serializer_class = MyAuthTokenSerializer
+    if coreapi is not None and coreschema is not None:
+        schema = ManualSchema(
+            fields=[
+                coreapi.Field(
+                    name='email',
+                    required=True,
+                    location='form',
+                    schema=coreschema.String(
+                        title="Email",
+                        description="Email valido para autenticacion"
+                    ),
+                ),
+                coreapi.Field(
+                    name='password',
+                    required=True,
+                    location='form',
+                    schema=coreschema.String(
+                        title="Contrasena",
+                        description="Contrasena valida para autenticacion"
+                    ),
+                ),
+            ],
+            encoding='application/json'
+        )
+
+
+obtain_auth_token = MyAuthToken.as_view()
