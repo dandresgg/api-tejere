ca348e06b9de3bde2bd6c0da4fc66b7be5406900
diego <diegogomez268@gmail.com>
2022-06-09 08:44:17 -0500

    models user app

---
+++ user_profile/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=20, null=True)
+    phone = models.PositiveIntegerField(default=0)
+    document = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return super().__str__()
