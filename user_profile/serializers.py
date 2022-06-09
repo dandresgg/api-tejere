bb83e5438cf3df141f74742a42ac00feec8e9e11
diego <diegogomez268@gmail.com>
2022-06-09 09:51:52 -0500

    user's serializer

--- user_profile/serializers.py
+++ user_profile/serializers.py
@@ -1,4 +1,8 @@
+from django.contrib.auth.models import User
+from django.core import validators
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from rest_framework.authtoken.models import Token
 
 from user_profile.models import Profile
 
@@ -7,3 +11,21 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ('id', 'user', 'address', 'phone', 'document')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    ''' create user registration '''
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
+    class Meta:
+        model = User
+        fields = ('id',  'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        Profile.objects.create(user=user)
+        Token.objects.create(user=user)
+        return user
