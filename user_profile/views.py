650f8f150dbe09287445bf842fb7c22be8793b01
diego <diegogomez268@gmail.com>
2022-06-11 12:32:58 -0500

    view my own auth verification with email

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,14 +1,17 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.authtoken import views as auth_views
 from rest_framework.compat import coreapi, coreschema
 from rest_framework.schemas import ManualSchema
+from rest_framework.response import Response
 
 from user_profile.models import Profile
-from user_profile.serializers import ProfileSerializer, UserSerializer, MyAuthTokenSerializer
+from user_profile.serializers import ProfileSerializer,\
+    UserSerializer, MyAuthTokenSerializer
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
@@ -16,6 +19,35 @@ class ProfileViewSet(viewsets.ModelViewSet):
     serializer_class = ProfileSerializer
     authentication_classes = (TokenAuthentication, )
     permission_classes = (IsAuthenticated, )
+
+    @action(detail=False, methods=['GET'])
+    def get_id(self, request):
+        user = request.user
+        profile = Profile.objects.get(user=user)
+        if not profile:
+            return Response('no user', status=status.HTTP_400_BAD_REQUEST)
+        return Response({'user_id': profile.id}, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['POST'])
+    def update_profile(self, request):
+        body = request.data['body']
+        user = request.user
+        profile = Profile.objects.get(user=user)
+        if body[0] == 'username':
+            profile.user.username = body[1]
+        if body[0] == 'email':
+            profile.user.email = body[1]
+        if body[0] == 'address':
+            profile.address = body[1]
+        if body[0] == 'phone':
+            profile.phone = body[1]
+        if body[0] == 'document':
+            profile.document = body[1]
+        if not profile:
+            return Response('no user', status=status.HTTP_400_BAD_REQUEST)
+        profile.save()
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class UserViewSet(viewsets.ModelViewSet):
