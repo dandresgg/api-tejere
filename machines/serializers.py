1a9573cce65a7ab4ea56a86bc0a16e99ffd89735
diego <diegogomez268@gmail.com>
2022-06-13 09:54:13 -0500

    serializer adding id to get reference of them

--- machines/serializers.py
+++ machines/serializers.py
@@ -12,13 +12,11 @@ class MachineSerializer(serializers.ModelSerializer):
 class SectorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sector
-        fields = ('machine', 'kind', 'img')
+        fields = ('id', 'machine', 'kind', 'img')
 
 
 class PartSerializer(serializers.ModelSerializer):
-    sector_name = serializers.CharField(source='sector.kind')
-
     class Meta:
         model = Part
-        field = ('sector_name', 'description', 'code',
-                 'reference', 'price', 'img', 'stock')
+        fields = ('id', 'sector', 'description', 'code',
+                  'reference', 'price', 'img', 'stock')
