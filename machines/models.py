7036d48fa2dd7599514be4492da803add74a41a8
diego <diegogomez268@gmail.com>
2022-06-13 09:54:42 -0500

    adding photo and all fields require part's models

--- machines/models.py
+++ machines/models.py
@@ -42,15 +42,17 @@ class Part(models.Model):
     sector = models.ForeignKey(Sector,
                                on_delete=models.CASCADE)
     description = models.TextField(max_length=150,
-                                   blank=True)
+                                   blank=False)
     code = models.CharField(max_length=20,
-                            blank=True)
+                            blank=False)
     reference = models.PositiveIntegerField(default=0,
-                                            blank=True)
+                                            blank=False)
     price = models.DecimalField(default=0,
                                 max_digits=10,
-                                decimal_places=2)
-    img = models.URLField(blank=True)
+                                decimal_places=2,
+                                blank=False)
+    img = models.URLField(blank=False)
+    photo = models.URLField(blank=False)
     stock = models.PositiveIntegerField(default=0)
 
     def __str__(self):
