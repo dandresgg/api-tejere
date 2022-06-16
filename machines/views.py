083661027c8d39364a94b673ca0c01f00205d5b9
diego <diegogomez268@gmail.com>
2022-06-16 07:20:53 -0500

    get element by id with get, but base on foreign key

--- machines/views.py
+++ machines/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
-from machines.models import Machine, Sector
-from machines.serializers import MachineSerializer, SectorSerializer
+from machines.models import Machine, Part, Sector
+from machines.serializers import MachineSerializer, PartSerializer, SectorSerializer
 
 
 class MachineViewSet(viewsets.ModelViewSet):
@@ -18,3 +20,34 @@ class SectorViewSet(viewsets.ModelViewSet):
     serializer_class = SectorSerializer
     authentication_classes = (TokenAuthentication, )
     permission_classes = (AllowAny, )
+
+    @action(detail=False, methods=['GET'])
+    def machine_sector(self, request):
+        m_id = self.request.query_params.get('m_id')
+        machine = Machine.objects.get(id=m_id)
+        sectors = Sector.objects.filter(machine=machine)
+        if sectors:
+            response = []
+            for sector in sectors:
+                response.append(SectorSerializer(sector).data)
+            return Response(response, status=status.HTTP_200_OK)
+        return Response('false', status=status.HTTP_400_BAD_REQUEST)
+
+
+class PartViewSet(viewsets.ModelViewSet):
+    queryset = Part.objects.all()
+    serializer_class = PartSerializer
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (AllowAny, )
+
+    @action(detail=False, methods=['GET'])
+    def sector_part(self, request):
+        s_id = self.request.query_params.get('s_id')
+        sector = Sector.objects.get(id=s_id)
+        parts = Part.objects.filter(sector=sector)
+        response = []
+        if parts:
+            for part in parts:
+                response.append(PartSerializer(part).data)
+            return Response(response, status=status.HTTP_200_OK)
+        return Response('false', status=status.HTTP_400_BAD_REQUEST)
