b9e43d9853113e546cfb2118169874f3fbb6cecb
diego <diegogomez268@gmail.com>
2022-06-20 11:54:49 -0500

    orders views

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import OrderSerializer
+from orders.models import Order
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    authentication_classes = (TokenAuthentication)
+    permission_classes = (IsAuthenticated)
