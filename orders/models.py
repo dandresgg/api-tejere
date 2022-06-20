3d7384ead97f30a4a16a5cc5bf3565b341862fa7
diego <diegogomez268@gmail.com>
2022-06-20 11:16:39 -0500

    orders models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE)
+    number = models.IntegerField(default=0)
+    data = models.JSONField(blank=True)
+    bill = models.FileField(upload_to='bills/')
