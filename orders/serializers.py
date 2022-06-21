1cd13e9709d66157ebf3a94f133821fa3501d607
diego <diegogomez268@gmail.com>
2022-06-21 11:31:26 -0500

    serializers adding state, send and created

--- orders/serializers.py
+++ orders/serializers.py
@@ -6,4 +6,5 @@ from orders.models import Order
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ('id', 'user', 'number', 'bill')
+        fields = ('id', 'user', 'number', 'bill',
+                  'data_json', 'state', 'send', 'created')
