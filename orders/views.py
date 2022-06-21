e0c7568c666685b9729d97b4a5ea5d16ae6f8f3c
diego <diegogomez268@gmail.com>
2022-06-21 07:45:03 -0500

    put correct authentication in view as tuple

--- orders/views.py
+++ orders/views.py
@@ -10,5 +10,5 @@ from orders.models import Order
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    authentication_classes = (TokenAuthentication)
-    permission_classes = (IsAuthenticated)
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
