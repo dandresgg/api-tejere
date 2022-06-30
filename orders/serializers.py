32978f664c605e0148040d546c275bf49e18bb08
diego <diegogomez268@gmail.com>
2022-06-30 10:15:33 -0500

    custom create part serilializer method

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from machines.models import Part
 
 from orders.models import Order
 
@@ -8,3 +9,12 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = ('id', 'user', 'number', 'bill',
                   'data_json', 'state', 'send', 'created')
+
+    def create(self, validated_data):
+        parts = validated_data['data_json']
+        for j_part in parts:
+            if j_part['qty'] > 0:
+                part = Part.objects.get(id=j_part['id'])
+                part.stock = part.stock - j_part['qty']
+                part.save()
+        return super().create(validated_data)
