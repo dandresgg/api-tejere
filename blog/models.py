1bc9fa16cd6c9f95d3d790b6002f49638bae05e9
diego <diegogomez268@gmail.com>
2022-07-02 09:12:35 -0500

    post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    body = RichTextField()
+    created = models.DateTimeField(auto_now=True)
+    modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
