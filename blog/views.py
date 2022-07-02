19f7cdcd3a7ccfe5267c74f9cbe2f37b7777b344
diego <diegogomez268@gmail.com>
2022-07-02 09:13:31 -0500

    post viewset

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from blog.models import Post
+from blog.serializers import PostSerializers
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializers
+    permission_classes = (AllowAny)
